3c4a54a3ea580180a204143e0a2e0518c67e7f3e
Vordgi <sasha2822222@gmail.com>
2024-04-17 22:45:59 +0400

    nc-8 format files by eslint

--- src/lib/classnames-minifier-postloader.ts
+++ src/lib/classnames-minifier-postloader.ts
@@ -1,18 +1,27 @@
-import type { LoaderContext } from 'webpack';
-import type ConverterMinified from './ConverterMinified';
-import fs from 'fs';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { LoaderContext } from "webpack";
+import type ConverterMinified from "./ConverterMinified";
+import fs from "fs";
 
-export default function (this: LoaderContext<{ classnamesMinifier: ConverterMinified }>, source: string, map: any, meta: any) {
-  const options = this.getOptions();
-  const classnamesMinifier = options.classnamesMinifier;
-  Object.entries(classnamesMinifier.dirtyСache).forEach(([resourcePath, data]) => {
-    if (data.type !== 'old') {
-      fs.writeFileSync(data.cachePath, `${resourcePath},${classnamesMinifier.lastIndex},${Object.entries(data.matchings).map(
-        ([key, value]) => (`${key}=${value}`)
-      ).join(',')}`)
-    }
-  })
+export default function (
+    this: LoaderContext<{ classnamesMinifier: ConverterMinified }>,
+    source: string,
+    map: any,
+    meta: any,
+) {
+    const options = this.getOptions();
+    const classnamesMinifier = options.classnamesMinifier;
+    Object.entries(classnamesMinifier.dirtyСache).forEach(([resourcePath, data]) => {
+        if (data.type !== "old") {
+            fs.writeFileSync(
+                data.cachePath,
+                `${resourcePath},${classnamesMinifier.lastIndex},${Object.entries(data.matchings)
+                    .map(([key, value]) => `${key}=${value}`)
+                    .join(",")}`,
+            );
+        }
+    });
 
-  this.callback(null, source, map, meta);
-  return;
+    this.callback(null, source, map, meta);
+    return;
 }
